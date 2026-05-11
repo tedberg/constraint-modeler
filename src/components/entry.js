26a9a3399e0a46611b23e0cc1e7f716975b9f220
Ted Bergeron <tedberg@triview.com>
2026-05-11 14:36:41 -0400

    feat: update entry.js for Vue 3 plugin API

--- src/components/entry.js
+++ src/components/entry.js
@@ -1,31 +1,10 @@
 import ConstraintModeler from './constraint-modeler/ui/ConstraintModeler.vue';
 
-// What should happen if the user installs the library as a plugin
-function install (Vue) {
-  if (install.installed) return;
-  install.installed = true;
-  Vue.component('ConstraintModeler', ConstraintModeler);
-}
-
-// Create module definition for Vue.use()
 const plugin = {
-  install
+  install (app) {
+    app.component('ConstraintModeler', ConstraintModeler);
+  }
 };
 
-// To auto-install when vue is found
-/* global window global */
-let GlobalVue = null;
-if (typeof window !== 'undefined') {
-  GlobalVue = window.Vue;
-} else if (typeof global !== 'undefined') {
-  GlobalVue = global.Vue;
-}
-if (GlobalVue) {
-  GlobalVue.use(plugin);
-}
-
-// Export components individually
 export { ConstraintModeler };
-
-// Export the library as a plugin
-export default { install: install };
+export default plugin;

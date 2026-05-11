f994e12df62220801357624f58b2d02417891282
Ted Bergeron <tedberg@triview.com>
2026-05-11 14:36:21 -0400

    feat: update router for Vue Router 4

--- src/demo/router.js
+++ src/demo/router.js
@@ -1,5 +1,4 @@
-import Vue from 'vue';
-import Router from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import Home from './views/Home.vue';
 import Simple from './views/Simple.vue';
 import Debug from './views/Debug.vue';
@@ -7,61 +6,21 @@ import WithProjection from './views/WithProjection.vue';
 import Persistent from './views/Persistent.vue';
 import Everything from './views/Everything.vue';
 
-Vue.use(Router);
-
-const router = new Router({
-  mode: 'history',
+const router = createRouter({
+  history: createWebHistory(),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-      meta: { title: 'Home' }
-    },
-    {
-      path: '/simple',
-      name: 'simple',
-      component: Simple,
-      meta: { title: 'Simple' }
-    },
-    {
-      path: '/debug',
-      name: 'debug',
-      component: Debug,
-      meta: { title: 'Debug' }
-    },
-    {
-      path: '/projection',
-      name: 'projection',
-      component: WithProjection,
-      meta: { title: 'Projection' }
-    },
-    {
-      path: '/persistent',
-      name: 'persistent',
-      component: Persistent,
-      meta: { title: 'Persistent' }
-    },
-    {
-      path: '/everything',
-      name: 'everything',
-      component: Everything,
-      meta: { title: 'Everything' }
-    },
-    {
-      path: '*',
-      component: Home,
-      meta: { title: 'Home' }
-    }
-  ]
+    { path: '/', name: 'home', component: Home, meta: { title: 'Home' } },
+    { path: '/simple', name: 'simple', component: Simple, meta: { title: 'Simple' } },
+    { path: '/debug', name: 'debug', component: Debug, meta: { title: 'Debug' } },
+    { path: '/projection', name: 'projection', component: WithProjection, meta: { title: 'Projection' } },
+    { path: '/persistent', name: 'persistent', component: Persistent, meta: { title: 'Persistent' } },
+    { path: '/everything', name: 'everything', component: Everything, meta: { title: 'Everything' } },
+    { path: '/:pathMatch(.*)*', redirect: '/simple' },
+  ],
 });
 
-router.beforeEach((to, from, next) => {
-  document.title = to.meta.title;
-  next();
-});
-
-router.afterEach((to, from) => {
+router.beforeEach((to) => {
+  document.title = to.meta.title || 'Constraint Modeler';
 });
 
 export default router;

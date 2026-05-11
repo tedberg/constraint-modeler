2bed21f9aaf9ddb31cd23b376885ef86d2601ab6
Ted Bergeron <tedberg@triview.com>
2026-05-11 09:40:25 -0400

    feat: add playwright.config.ts

---
+++ playwright.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests/playwright',
+  fullyParallel: false,
+  reporter: [['html', { open: 'never' }]],
+  use: {
+    baseURL: 'http://localhost:8080',
+    testIdAttribute: 'data-test',
+    trace: 'on-first-retry',
+    video: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+  webServer: {
+    command: 'npm run serve',
+    url: 'http://localhost:8080',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120000,
+  },
+});
